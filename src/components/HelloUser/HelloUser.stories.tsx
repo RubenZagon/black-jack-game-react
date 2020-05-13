0b19adb6a55e531bffc1f8adf042b926bf6887c1
Rubén Zagon <ruben.zagon@gmail.com>
2020-05-13 10:28:54 +0100

    Add Knobs and Viewport

--- src/components/HelloUser/HelloUser.stories.tsx
+++ src/components/HelloUser/HelloUser.stories.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import HelloUser from "./HelloUser";
+import {text, withKnobs} from "@storybook/addon-knobs";
 
 
 export default {
   title: 'HelloUser',
+  decorators: [withKnobs]
 };
 
-export const withName = () => <HelloUser name={'Ruben'} />;
+export const withName = () => <HelloUser name={text("Nombre", "Rubén")} />;
