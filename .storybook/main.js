6fa401cc1876dff8c3f813f6835877d6e2361644
Rubén Zagon <ruben.zagon@gmail.com>
2020-05-13 10:58:54 +0100

    Add a11y

--- .storybook/main.js
+++ .storybook/main.js
@@ -5,6 +5,7 @@ module.exports = {
     '@storybook/addon-actions',
     '@storybook/addon-links',
     '@storybook/addon-knobs/register',
-    '@storybook/addon-viewport/register'
+    '@storybook/addon-viewport/register',
+    '@storybook/addon-a11y/register'
   ],
 };
--- src/components/HelloUser/HelloUser.stories.tsx
+++ src/components/HelloUser/HelloUser.stories.tsx
@@ -1,11 +1,12 @@
 import React from 'react';
 import HelloUser from "./HelloUser";
 import {text, withKnobs} from "@storybook/addon-knobs";
+import {withA11y} from "@storybook/addon-a11y";
 
 
 export default {
   title: 'HelloUser',
-  decorators: [withKnobs]
+  decorators: [withKnobs, withA11y]
 };
 
 export const withName = () => <HelloUser name={text("Nombre", "Rubén")} />;
